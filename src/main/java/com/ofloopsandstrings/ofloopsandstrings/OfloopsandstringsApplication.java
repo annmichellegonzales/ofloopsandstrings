5de9140a0334b73bbb273e6831ef452f78324a1f
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-30 09:53:02 -0500

    .gitignore

---
+++ src/main/java/com/ofloopsandstrings/ofloopsandstrings/OfloopsandstringsApplication.java
@@ -0,0 +1,13 @@
+package com.ofloopsandstrings.ofloopsandstrings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfloopsandstringsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfloopsandstringsApplication.class, args);
+    }
+
+}
