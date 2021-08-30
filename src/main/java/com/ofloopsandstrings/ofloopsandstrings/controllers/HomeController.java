061ab78310d51cc5ea1907a04d89944a79dade69
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-30 14:38:57 -0500

    home

--- src/main/java/com/ofloopsandstrings/ofloopsandstrings/controllers/HomeController.java
+++ src/main/java/com/ofloopsandstrings/ofloopsandstrings/controllers/HomeController.java
@@ -2,13 +2,11 @@ package com.ofloopsandstrings.ofloopsandstrings.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    @ResponseBody
     public String welcome() {
         return "home";
     }
