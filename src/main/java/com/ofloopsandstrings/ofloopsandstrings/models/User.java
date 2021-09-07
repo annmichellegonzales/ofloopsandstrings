4432edb2144c1419c3c19e3ede58be2fd064e550
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-09-06 19:27:34 -0500

    getters/setters, constructor

--- src/main/java/com/ofloopsandstrings/ofloopsandstrings/models/User.java
+++ src/main/java/com/ofloopsandstrings/ofloopsandstrings/models/User.java
@@ -2,6 +2,7 @@ package com.ofloopsandstrings.ofloopsandstrings.models;
 
 import org.springframework.stereotype.Controller;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
@@ -12,5 +13,40 @@ public class User {
     @GeneratedValue
     private long id;
 
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
